f959fdd4d3893de8c7efec8a007acbceb7cf6eed
SevarJafarli <sevar.jafarli.std@bhos.edu.az>
2021-11-27 13:59:25 +0400

    connectiviy

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Flutter News App
 
 ### Flutter News App with NewsApi
+
+Used Getx for Dynamic Theme(Light and Dark themes) and Navigation
+Used Provider for Network Connectivity
--- lib/presentation/pages/category_news_page.dart
+++ lib/presentation/pages/category_news_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_news/infrastructure/providers/connectivity_provider.dart';
 import 'package:flutter_news/infrastructure/services/api_service.dart';
 import 'package:flutter_news/infrastructure/models/article_model.dart';
 import 'package:flutter_news/presentation/widgets/article_list.dart';
+import 'package:flutter_news/presentation/widgets/no_internet_connection.dart';
 import 'package:get/route_manager.dart';
+import 'package:provider/provider.dart';
 
 class CategoryNewsPage extends StatefulWidget {
   const CategoryNewsPage({Key? key, required this.categoryName})
@@ -15,28 +18,39 @@ class CategoryNewsPage extends StatefulWidget {
 
 class _CategoryNewsPageState extends State<CategoryNewsPage> {
   ApiService apiService = ApiService();
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<ConnectivityProvider>(context, listen: false).startMonitoring();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppBar(context),
-      body: FutureBuilder(
-        future: apiService.fetchNewsByCategory(
-          widget.categoryName,
-        ),
-        builder: (context, AsyncSnapshot<List<ArticleModel>> snapshot) {
-          if (snapshot.hasData) {
-            return ArticlesList(
-                refreshPage: _refreshPage, articles: snapshot.data!);
-          } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+        appBar: _buildAppBar(context),
+        body: Consumer<ConnectivityProvider>(
+            builder: (consumerContext, model, child) {
+          return model.isOnline ? _buildBody() : const NoInternetConnection();
+        }));
+  }
+
+  FutureBuilder<List<ArticleModel>> _buildBody() {
+    return FutureBuilder(
+      future: apiService.fetchNewsByCategory(
+        widget.categoryName,
       ),
+      builder: (context, AsyncSnapshot<List<ArticleModel>> snapshot) {
+        if (snapshot.hasData) {
+          return ArticlesList(
+              refreshPage: _refreshPage, articles: snapshot.data!);
+        } else if (snapshot.hasError) {
+          return const Center(child: NoInternetConnection());
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 
